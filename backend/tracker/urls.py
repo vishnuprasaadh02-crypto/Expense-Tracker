e71b29c8e96e604d1478e3731f0cb1e3d5bf6203
vishnuprasaadh02-crypto <vishnuprasaadh02@gmail.com>
2026-09-17 01:04:08 +0530

    Create urls.py

---
+++ backend/tracker/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from .views import ExpenseViewSet
+
+router = DefaultRouter()
+router.register(r'expenses', ExpenseViewSet, basename='expense')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
